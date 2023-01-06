ccf67b3a78359fa3d2b226c8745b3e9d5d6ccd63
steff <steffyoroian@gmail.com>
2023-01-06 20:30:21 +0200

    Add project files.

---
+++ Oroian_Stefania_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Oroian_Stefania_Lab11.Data;
+
+namespace Oroian_Stefania_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Oroian_Stefania_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Oroian_Stefania_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
